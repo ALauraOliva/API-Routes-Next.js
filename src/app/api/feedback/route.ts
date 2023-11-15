35646954505f54b6a7c89c1eca958b1508d047c1
ALauraOliva <andrea_carol00@hotmail.com>
2023-11-15 11:11:48 -0500

    first commit

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -8,7 +8,7 @@ type Feedback = {
 
 export async function POST(request: Request) {
   const data: Feedback = await request.json();
-  console.log("data: ", data);
+  console.log("data : ", data);
 
   const { name, email, message } = data;
 
